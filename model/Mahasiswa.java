9b1f34d02444f1cb759d9d559a7a5499651547f1
mtaufikrakhman <62371800+mtaufikrakhman@users.noreply.github.com>
2020-03-25 12:11:41 +0700

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ==========");
+  System.out.println("NIDN : 18630581");
+  System.out.println("NAMA : M.TAUFIK RAKHMAN");
+  System.out.println("============================");
+ }
+}
---
+++ model/MataKuliah.java
@@ -0,0 +1,11 @@
+package model;
+
+public class MataKuliah{
+ public static void tampilInfo(){
+  System.out.println();
+  System.out.println("== Data Mata Kuliah =============");
+  System.out.println("Kode : TIF4302");
+  System.out.println("NAMA : Pemrograman Berbasis Objek");
+  System.out.println("=================================");
+ }
+}
